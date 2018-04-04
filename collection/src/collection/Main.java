56b833e4ccbfb120dd5b147a5bb162c6c62b46e4
buberchik <buberchik@192.168.1.2>
2018-04-04 23:41:48 +0300

    12345

---
+++ collection/src/collection/Main.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+	private static ArrayList<Mobile> mobiles = new ArrayList<Mobile>();
+	private static Random r = new Random();
+	
+	public static void main(String[] args) {
+		for(int i=0;i<800000;i++) {
+			mobiles.add(new Mobile(r.nextInt(9999999), "sdsdsdsd"));
+		}
+		for(Mobile m:mobiles) {
+			System.out.println(m.getNumber()+ " "+ m.getName());
+		}
+	}
+}
+				
+
+
